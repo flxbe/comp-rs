5c7bbabc0f6e7a231f843848e0e065c391c3ea63
Felix Bernhardt <felix.bernhardt@mailbox.org>
2018-03-11 12:28:56 +0100

    draw a white pixel

--- src/drm.rs
+++ src/drm.rs
@@ -1,11 +1,15 @@
 extern crate libc;
+extern crate memmap;
 
+use std::{thread, time};
 use std::fs::{File, OpenOptions};
 use std::os::unix::io::AsRawFd;
 
 use self::libc::ioctl;
 use super::drm_const::*;
 use super::ffi::*;
+
+use self::memmap::{MmapOptions};
 
 pub trait Pointer {
     fn as_ptr(&mut self) -> *mut Self {
@@ -27,76 +31,36 @@ fn create_buffer<T>(size: u32) -> Vec<T> {
     buffer
 }
 
-#[derive(Debug)]
-pub struct Device {
-    fd: File,
-    connectors: Vec<Connector>,
-    _drm_mode_card_res: drm_mode_card_res
-}
+pub fn open(path: &str) {
+    let fd = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(path).unwrap();
 
-impl Device {
-    pub fn open(path: &str) -> Device {
-        let fd = OpenOptions::new()
-            .read(true)
-            .write(true)
-            .open(path).unwrap();
+    let mut res = drm_mode_card_res::default();
+    unsafe { ioctl(fd.as_raw_fd(), DRM_IOCTL_MODE_GETRESOURCES, res.as_ptr()) };
 
-        let mut res = drm_mode_card_res::default();
-        unsafe { ioctl(fd.as_raw_fd(), DRM_IOCTL_MODE_GETRESOURCES, res.as_ptr()) };
+    /**
+     * read device infos
+     **/
+    let mut fb_ids: Vec<u64> = create_buffer(res.count_fbs);
+    let mut crtc_ids: Vec<u64> = create_buffer(res.count_crtcs);
+    let mut connector_ids: Vec<u64> = create_buffer(res.count_connectors);
+    let mut encoder_ids: Vec<u64> = create_buffer(res.count_encoders);
+    res.fb_id_ptr = fb_ids.as_mut_ptr() as u64;
+    res.crtc_id_ptr = crtc_ids.as_mut_ptr() as u64;
+    res.connector_id_ptr = connector_ids.as_mut_ptr() as u64;
+    res.encoder_id_ptr = encoder_ids.as_mut_ptr() as u64;
+    unsafe { ioctl(fd.as_raw_fd(), DRM_IOCTL_MODE_GETRESOURCES, res.as_ptr()) };
 
-        // create buffers and read device infos
-        let mut fb_ids: Vec<u64> = create_buffer(res.count_fbs);
-        let mut crtc_ids: Vec<u64> = create_buffer(res.count_crtcs);
-        let mut connector_ids: Vec<u64> = create_buffer(res.count_connectors);
-        let mut encoder_ids: Vec<u64> = create_buffer(res.count_encoders);
-        res.fb_id_ptr = fb_ids.as_mut_ptr() as u64;
-        res.crtc_id_ptr = crtc_ids.as_mut_ptr() as u64;
-        res.connector_id_ptr = connector_ids.as_mut_ptr() as u64;
-        res.encoder_id_ptr = encoder_ids.as_mut_ptr() as u64;
-        unsafe { ioctl(fd.as_raw_fd(), DRM_IOCTL_MODE_GETRESOURCES, res.as_ptr()) };
-
-        let mut connectors: Vec<Connector> = connector_ids.iter().map(|&id| Connector::new(&fd, id)).collect();
-        for connector in connectors.iter_mut() {
-            connector.create_fb(&fd);
-        }
-
-        return Device {
-            fd,
-            connectors,
-            _drm_mode_card_res: res
-        }
-    }
-
-    fn set_master(&mut self) {
-        unsafe { ioctl(self.fd.as_raw_fd(), DRM_IOCTL_SET_MASTER, 0) };
-    }
-}
-
-#[derive(Debug)]
-pub struct Connector {
-    modes: Vec<drm_mode_modeinfo>,
-    props: Vec<u64>,
-    prop_values: Vec<u64>,
-    encoders: Vec<u64>,
-
-    fb: Option<FrameBuffer>,
-    encoder: Encoder,
-
-    _connector: drm_mode_get_connector
-}
-
-impl Connector {
-    pub fn new(fd: &File, id: u64) -> Connector {
-        println!("inspect connector with id {}", id);
-
+    let mut fbs: Vec<FrameBuffer> = Vec::new();
+    for &connector_id in connector_ids.iter() {
+        /**
+         * read connector infos
+         **/
         let mut connector = drm_mode_get_connector::default();
-        connector.connector_id = id as u32;
-
+        connector.connector_id = connector_id as u32;
         unsafe { ioctl(fd.as_raw_fd(), DRM_IOCTL_MODE_GETCONNECTOR, connector.as_ptr()) };
-
-        println!("total modes: {}", connector.count_modes);
-        println!("total props: {}", connector.count_props);
-        println!("total encoders: {}", connector.count_encoders);
 
         let mut modes: Vec<drm_mode_modeinfo> = create_buffer(connector.count_modes);
         let mut props: Vec<u64> = create_buffer(connector.count_props);
@@ -117,25 +81,12 @@ impl Connector {
             println!("{}", name);
         }
 
-        let encoder = Encoder::new(fd, connector.encoder_id);
-
-        Connector {
-            modes,
-            props,
-            prop_values,
-            encoders,
-
-            fb: None,
-            encoder,
-
-            _connector: connector
-        }
-    }
-
-    pub fn create_fb(&mut self, fd: &File) {
+        /**
+         * create the framebuffer
+         **/
         let mut buffer_config = drm_mode_create_dumb {
-            width: self.modes[0].hdisplay as u32,
-            height: self.modes[0].vdisplay as u32,
+            width: modes[0].hdisplay as u32,
+            height: modes[0].vdisplay as u32,
             bpp: 32,
             flags: 0,
             pitch: 0,
@@ -155,81 +106,50 @@ impl Connector {
         };
         unsafe { ioctl(fd.as_raw_fd(), DRM_IOCTL_MODE_ADDFB, fb_cmd.as_ptr()) };
 
+        /**
+         * mmap the framebuffer
+         **/
         let mut map_config = drm_mode_map_dumb::default();
         map_config.handle = buffer_config.handle;
         unsafe { ioctl(fd.as_raw_fd(), DRM_IOCTL_MODE_MAP_DUMB, map_config.as_ptr()) };
 
-        let mut connectors = vec![self._connector.connector_id];
-        let mut crtc = &mut self.encoder._crtc;
-        crtc.fb_id = fb_cmd.fb_id;
-        crtc.set_connectors_ptr = connectors.as_ptr() as u64;
-        crtc.count_connectors = connectors.len() as u32;
-    }
-}
+        let mut mmap = unsafe {
+            MmapOptions::new()
+                .offset(map_config.offset as usize)
+                .len(buffer_config.size as usize)
+                .map_mut(&fd)
+                .unwrap()
+        };
 
-#[derive(Debug)]
-struct Encoder {
-    _encoder: drm_mode_get_encoder,
-    _crtc: drm_mode_crtc
-}
-
-impl Encoder {
-    pub fn new(fd: &File, id: u32) -> Encoder {
-        println!("inspect encoder with id {}", id);
-
+        mmap[0] = 0xff;
+        mmap[1] = 0xff;
+        mmap[2] = 0xff;
+        mmap[3] = 0xff;
+            
+        /**
+         * initialize the crtc
+         **/
         let mut encoder = drm_mode_get_encoder::default();
-        encoder.encoder_id = id;
+        encoder.encoder_id = connector.encoder_id;
         unsafe { ioctl(fd.as_raw_fd(), DRM_IOCTL_MODE_GETENCODER, encoder.as_ptr()) };
 
-        println!("inspect crtc with id {}", encoder.crtc_id);
         let mut crtc = drm_mode_crtc::default();
         crtc.crtc_id = encoder.crtc_id;
         unsafe { ioctl(fd.as_raw_fd(), DRM_IOCTL_MODE_GETCRTC, crtc.as_ptr()) };
 
-        Encoder {
-            _encoder: encoder,
-            _crtc: crtc
-        }
+        let mut connectors = vec![connector_id];
+        crtc.fb_id = fb_cmd.fb_id;
+        crtc.set_connectors_ptr = connectors.as_ptr() as u64;
+        crtc.count_connectors = connectors.len() as u32;
+        crtc.mode_valid = 1;
+        unsafe { ioctl(fd.as_raw_fd(), DRM_IOCTL_MODE_SETCRTC, crtc.as_ptr()) };
+
+        thread::sleep(time::Duration::from_millis(1000));
     }
 }
-
-pub fn open(path: &str) -> Device {
-    let fd = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .open(path).unwrap();
-
-    let mut res = drm_mode_card_res::default();
-    unsafe { ioctl(fd.as_raw_fd(), DRM_IOCTL_MODE_GETRESOURCES, res.as_ptr()) };
-
-    // create buffers and read device infos
-    let mut fb_ids: Vec<u64> = create_buffer(res.count_fbs);
-    let mut crtc_ids: Vec<u64> = create_buffer(res.count_crtcs);
-    let mut connector_ids: Vec<u64> = create_buffer(res.count_connectors);
-    let mut encoder_ids: Vec<u64> = create_buffer(res.count_encoders);
-    res.fb_id_ptr = fb_ids.as_mut_ptr() as u64;
-    res.crtc_id_ptr = crtc_ids.as_mut_ptr() as u64;
-    res.connector_id_ptr = connector_ids.as_mut_ptr() as u64;
-    res.encoder_id_ptr = encoder_ids.as_mut_ptr() as u64;
-    unsafe { ioctl(fd.as_raw_fd(), DRM_IOCTL_MODE_GETRESOURCES, res.as_ptr()) };
-
-    let mut connectors: Vec<Connector> = connector_ids.iter().map(|&id| Connector::new(&fd, id)).collect();
-    for connector in connectors.iter_mut() {
-        connector.create_fb(&fd);
-    }
-
-    return Device {
-        fd,
-        connectors,
-        _drm_mode_card_res: res
-    }
-}
-
 
 #[derive(Debug)]
 struct FrameBuffer {
-    ptr: u64,
-    width: u32,
-    height: u32
+    fb_cmd: drm_mode_fb_cmd
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
 
+
 #![allow(unused)]
 mod ffi;
 mod drm_const;
 mod drm;
 
-use drm::Device;
-
 fn main() {
-    let mut dev = Device::open("/dev/dri/card0");
+    drm::open("/dev/dri/card0");
 }
